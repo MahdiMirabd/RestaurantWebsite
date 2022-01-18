8aff4b94cd1f717407c6cf289516a52b39baa895
Hestre <zhac092@live.rhul.ac.uk>
2022-01-18 19:24:33 +0000

    adding the Menu repository which will allow the retrieval of menu data from the database.

---
+++ src/main/java/uk/ac/rhul/cs2810/restaurantsystem/repository/MenuRepository.java
@@ -0,0 +1,16 @@
+package uk.ac.rhul.cs2810.restaurantsystem.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import uk.ac.rhul.cs2810.restaurantsystem.model.Menu;
+
+/**
+ * Provides the means for storing, retrieving,
+ * searching, updating and deleting objects in the database.
+ *
+ * Menu - the model name.
+ * Long - the primary key of the table
+ */
+@Repository
+public interface MenuRepository extends JpaRepository<Menu, Long> {
+}
