6b41c4840dc6ed4bff089a14a90917bed2c77f2a
Hestre <zhac092@live.rhul.ac.uk>
2022-01-18 15:23:46 +0000

    The initial spring project for the restaurant management system.

---
+++ src/main/java/uk/ac/rhul/cs2810/restaurantsystem/RestaurantSystemApplication.java
@@ -0,0 +1,13 @@
+package uk.ac.rhul.cs2810.restaurantsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestaurantSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestaurantSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/uk/ac/rhul/cs2810/restaurantsystem/RestaurantSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package uk.ac.rhul.cs2810.restaurantsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestaurantSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
