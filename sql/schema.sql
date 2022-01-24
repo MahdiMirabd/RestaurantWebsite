c41510a278c83e53edc19dc5ba4cf41ae3099d78
22am <amirreza.amiri.2020@live.rhul.ac.uk>
2022-01-24 13:58:31 +0000

    Squashed commit of the following:
    commit 54bcacbd68faeb580b702a9880868419b84398b6
    Author: M-D-Junior <59103723+M-D-Junior@users.noreply.github.com>
    Date:   Mon Jan 24 13:45:37 2022 +0000
    
        HTML and CSS update
    
        I have added some more pages and linked each web page together. I have also added some html and css to those pages.
    
    commit 507212c7e999b65021e5952d8a762ef51f4b5aea
    Author: yxngrei <74990865+yxngrei@users.noreply.github.com>
    Date:   Mon Jan 24 13:34:20 2022 +0000
    
        Update schema.sql
    
        Added a table for allergies and created a relation between it and the menu table

--- sql/schema.sql
+++ sql/schema.sql
@@ -2,8 +2,14 @@ CREATE TABLE menu (
     id SERIAL PRIMARY KEY,
     name VARCHAR (50) NOT NULL,
     price NUMERIC (12, 2) NOT NULL,
-    calories NUMERIC NOT NULL
-    /*allergen VARCHAR,*/
+    calories NUMERIC NOT NULL,
+    allergies VARCHAR (50),
+    FOREIGN KEY (allergies) REFERENCES allergies(name)
+);
+
+CREATE TABLE allergies (
+    name VARCHAR (50) NOT NULL PRIMARY KEY,
+    description NUMERIC VARCHAR(50) NOT NULL,
 );
 
 INSERT INTO menu (name, price, calories)
