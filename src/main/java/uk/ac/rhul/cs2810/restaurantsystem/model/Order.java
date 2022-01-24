8ed4954de312c904cec92452f80bb77f6ab52965
Hestre <zhac092@live.rhul.ac.uk>
2022-01-24 18:36:15 +0000

    Refactoring file names and structure for consistency.

--- src/main/java/uk/ac/rhul/cs2810/restaurantsystem/model/Order.java
+++ src/main/java/uk/ac/rhul/cs2810/restaurantsystem/model/Order.java
@@ -1,7 +1,9 @@
-package uk.ac.rhul.cs2810.restaurantsystem;
+package uk.ac.rhul.cs2810.restaurantsystem.model;
 
 import java.util.ArrayList;
 import uk.ac.rhul.cs2810.restaurantsystem.model.Menu;
+
+import javax.persistence.Entity;
 
 /**
  * Creates an object storing the details of the order
@@ -9,13 +11,13 @@ import uk.ac.rhul.cs2810.restaurantsystem.model.Menu;
  * 
  * @author Rayan
  */
-public class MenuOrdering {
+public class Order {
 
     private static int orderNo = 0; // The id of the order
     private ArrayList<Menu> orderList; // The list of all the items that the customer ordered
     private float orderPrice; // The total price of all items the customer ordered
 
-    public MenuOrdering(){
+    public Order(){
         orderNo++;
         //orderList = ... get the list of objects from the item basket
         //orderPrice = ... get the total price of the objects in the item basket
