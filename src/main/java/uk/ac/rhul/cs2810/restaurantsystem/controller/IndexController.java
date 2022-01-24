feb16400e3ef6241b929dafff96b1eb7bf431520
22am <amirreza.amiri.2020@live.rhul.ac.uk>
2022-01-24 15:30:51 +0000

    IndexController created class for index.html page.

---
+++ src/main/java/uk/ac/rhul/cs2810/restaurantsystem/controller/IndexController.java
@@ -0,0 +1,17 @@
+package uk.ac.rhul.cs2810.restaurantsystem.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+@RequestMapping(value = {"/index"})
+public class IndexController {
+    @GetMapping(value = {""})
+    public ModelAndView index() {
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("index");
+        return modelAndView;
+    }
+}
