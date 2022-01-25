e4132f0a7bdd1524cdf8c928ab28cc1f07e919b1
MahdiMirab <75266227+MahdiMirab@users.noreply.github.com>
2022-01-25 17:30:19 +0000

    Update schema.sql
    Added descriptions for each menu item on the ingredients that are contained inside. added allergies for some other items.

--- sql/schema.sql
+++ sql/schema.sql
@@ -14,16 +14,16 @@ CREATE TABLE allergies (
 );
 
 INSERT INTO menu (name, price, calories, allergies, ingredients)
-VALUES  ('Burrito', 20.50, 90, 'Milk'),
-        ('Nachos', 10.50, 100, 'Milk'),
-        ('Tacos', 15, 120, 'Milk'),
-        ('Quesadillas', 13, 80, 'Milk'),
-        ('Burger', 10, 50, 'Milk'),
-        ('Chicken Wrap', 10, 30),
-        ('Beef Wrap', 10, 30),
-        ('Fish Burger', 10, 25, 'Fish'),
-        ('Egg Breakfast', 10, 15, 'Egg'),
-        ('Salsa', 5, 5, 'Gluten');
+VALUES  ('Burrito', 20.50, 90, 'Milk', 'Burrito that contains beef shredded with nacho cheese and spicy rice'),
+        ('Nachos', 10.50, 100, 'Milk', 'Nachos with nacho cheese over the top'),
+        ('Tacos', 15, 120, 'Milk','Tacos containing beef shredded with cheese and tomato'),
+        ('Quesadillas', 13, 80, 'Milk', 'Quesadillia containing cheese in a soft crunchy bread'),
+        ('Burger', 10, 50, 'Milk', 'Burger containing cheese and a beef patty'),
+        ('Chicken Wrap', 10, 30 'Milk', 'Chicken wrap containing shredded cheese and lettuce with tomato'),
+        ('Beef Wrap', 10, 30, 'Milk', 'Beef Wrap containing shredded cheese and lettuce with tomato'),
+        ('Fish Burger', 10, 25, 'Fish','Fish burger with nothing extra added'),
+        ('Egg Breakfast', 10, 15, 'Egg','Simple egg breakfast'),
+        ('Salsa', 5, 5, 'Gluten','Salsa dip containing tomato and fresh chilli peppers');
 INSERT INTO allergies(name, description)
 VALUES  ('Celery', "Contains Celery"),
         ('Gluten', "Contains Gluten"),
