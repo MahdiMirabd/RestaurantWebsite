b093df7f5ab1c0210d3c9670c0a490d15bbf09f8
22am <amirreza.amiri.2020@live.rhul.ac.uk>
2022-01-26 21:14:15 +0000

    Changed the styling of order page and Nav bar.
    The shopping cart icon is now included in the Nav bar to be accessible in all pages.

--- src/main/resources/static/javascript/header.js
+++ src/main/resources/static/javascript/header.js
@@ -29,7 +29,23 @@ class Header extends HTMLElement {
                     'padding-bottom: 5px;' +
                     'box-shadow: inset 0 -2px 0 0 #0a0a23;' +
                 '} ' +
+                '.shopping-cart-button {' +
+                    'background-color: white;' +
+                    'border: 2px solid Dodgerblue;' +
+                    'color: DodgerBlue;' +
+                    'padding: 12px 16px;' +
+                    'font-size: 16px;' +
+                    'cursor: pointer;' +
+                    'position:absolute;' +
+                    'top:20px;' +
+                    'right:85px;' +
+                  '} ' +
+                  '.shopping-cart-button:hover {' +
+                    'background-color: DodgerBlue;' +
+                    'color: white;' +
+                  '} ' +
             '</style> ' +
+            '<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">' +
             '<header> ' +
                 '<nav> ' +
                     '<ul>' +
@@ -39,6 +55,7 @@ class Header extends HTMLElement {
                         '<li class="log"><a href="/login">Login</a></li>' +
                     '</ul> ' +
                 '</nav>' +
+                '<button class="shopping-cart-button"><i class="fa fa-shopping-cart"></i></button>' +
             '</header>';
     }
 }
