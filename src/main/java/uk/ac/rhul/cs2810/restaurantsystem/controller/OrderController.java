f40904e59ed9e0e34d46e32494a031f853da993f
22am <amirreza.amiri.2020@live.rhul.ac.uk>
2022-01-26 17:52:32 +0000

    Order page will now list the food.
    The order page will only list the foods in the menu table and their prices, with a quantity dropdown button and a add-to-cart button next to each one.

--- src/main/java/uk/ac/rhul/cs2810/restaurantsystem/controller/OrderController.java
+++ src/main/java/uk/ac/rhul/cs2810/restaurantsystem/controller/OrderController.java
@@ -1,17 +1,23 @@
 package uk.ac.rhul.cs2810.restaurantsystem.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.servlet.ModelAndView;
+
+import uk.ac.rhul.cs2810.restaurantsystem.repository.MenuRepository;
+import uk.ac.rhul.cs2810.restaurantsystem.repository.OrderRepository;
 
 @Controller
 @RequestMapping(value = {"/order"})
 public class OrderController {
+    @Autowired
+    private MenuRepository menuRepository;
     @GetMapping(value = {""})
-    public ModelAndView order() {
-        ModelAndView modelAndView = new ModelAndView();
-        modelAndView.setViewName("order");
-        return modelAndView;
+    public String findAll(Model model) {
+
+        model.addAttribute("items", menuRepository.findAll());
+        return "order";
     }
 }
