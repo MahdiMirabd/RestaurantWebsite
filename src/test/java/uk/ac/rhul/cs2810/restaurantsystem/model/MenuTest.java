3ea36a6eb8d86798c756c0e7c60b71858af22ac0
Hestre <zhac092@live.rhul.ac.uk>
2022-01-27 21:38:18 +0000

    Refactoring. Refactoring unit test for null objects and setters.

--- src/test/java/uk/ac/rhul/cs2810/restaurantsystem/model/MenuTest.java
+++ src/test/java/uk/ac/rhul/cs2810/restaurantsystem/model/MenuTest.java
@@ -8,23 +8,18 @@ import static org.junit.jupiter.api.Assertions.*;
 class MenuTest {
     private Menu menu;
 
-    /*@Test
+    @Test
     void testNullObjects() {
         menu = new Menu();
        assertNull(menu.getName(), "No arguments passed to the constructor.");
     }
 
-   // @Test
-    void testConstructorWithArgs() {
-        menu = new Menu(1, "Burrito", 20.5F, 90.0F);
+   @Test
+    void testLombokAnnotations() {
+        menu = new Menu();
+        menu.setName("Burrito");
+        menu.setPrice(20.5F);
         assertEquals(menu.getPrice(), 20.5, "Could not get the item price.");
         assertNotNull(menu.getName(), "Could not get the item name.");
     }
-
-    //@Test
-    void testNullPointerException() {
-        assertThrows(NullPointerException.class, () -> menu = new Menu(1, null, 20.5F, 90.0F)
-                , "This field cannot be null." );
-    }*/
-
 }
