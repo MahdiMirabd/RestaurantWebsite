cbd191d8c259f4a382889de86b53b5ae0ccdba7e
Rayan111001 <zjac382@live.rhul.ac.uk>
2022-01-27 22:03:59 +0000

    Added functionality for the cart. Can add to cart and change quantity with the total price changing in real time as well as a remove button to get rid of items in the cart.

--- src/main/resources/static/javascript/order.js
+++ src/main/resources/static/javascript/order.js
@@ -4,12 +4,43 @@ var totalPrice = 0;
 // Add to cart button
 // It button will multiply the price of the item by the quantity and add it to the total price.
 $().ready(function () {
-    $(".add-to-cart-btn").click(function () {
+    $(".add-to-cart-btn").click(function (event) {
+        buttonClicked = event.target;
+        var name = $(this).closest('tr').find('.row-name').text();
         var quantity = $(this).closest('tr').find('input.qty').val();
         var price = $(this).closest('tr').find('.row-price').text();
-        totalPrice += (parseInt(quantity) * parseFloat(price));
+        if (quantity != 0) {
+            addItemToCart(name,quantity,price);
+            updateCartTotal();
+        }
     });
 });
+
+function addItemToCart(name, quantity, price) {
+    var cartRow = document.createElement('div');
+    cartRow.classList.add('cart-row');
+    var cartItems = document.getElementsByClassName('cart-items')[0];
+    var cartItemNames = cartItems.getElementsByClassName('cart-item-title');
+    for (var i = 0; i < cartItemNames.length; i++) {
+        if (cartItemNames[i].innerText == name) {
+            alert('This item has already been added, change the quantity in the cart.');
+            return;
+        }
+    }
+    var cartRowContents = `
+        <div class="cart-item cart-column">
+            <span class="cart-item-title">${name}</span>
+        </div>
+        <span class="cart-price cart-column">${price}</span>
+        <div class="cart-quantity">
+            <input class="cart-quantity-input" type="number" value="${quantity}">
+            <button class="btn btn-remove" type="button">REMOVE</button>
+        </div>`
+        cartRow.innerHTML = cartRowContents;
+    cartItems.append(cartRow);
+    cartRow.getElementsByClassName('btn-remove')[0].addEventListener('click', removeItemFromCart);
+    cartRow.getElementsByClassName('cart-quantity-input')[0].addEventListener('change', quantityChanged);
+}
 
 // "-" button
 // It decrements the quantity of the item in that row ONLY if the integer value of quantity is bigger than 0,
@@ -25,7 +56,7 @@ $().ready(function () {
     });
 });
 
-// "=" button
+// "+" button
 // It increments the quantity of the item in that row.
 $().ready(function () {
     $(".increment-qty-btn").click(function () {
@@ -43,3 +74,56 @@ $().ready(function () {
     $(".shopping-cart-button").click(function () {
         window.alert("Total cost :   " + totalPrice.toString());    });
 });
+
+//Waits for a remove button to be clicked
+$().ready(function() {
+    var removeBtn = document.getElementsByClassName('btn-remove');
+    for (var i = 0; i < removeBtn.length; i++) {
+        var input = removeBtn[i];
+        input.addEventListener('click', removeItemFromCart)
+    }
+});
+
+//checks if the menu has been updated so the price can then be updated
+$().ready(function() {
+    var quantityInputs = document.getElementsByClassName('cart-quantity-input');
+    for (var i = 0; i < quantityInputs.length; i++) {
+        var input = quantityInputs[i];
+        input.addEventListener('change', quantityChanged)
+    }
+});
+
+//Removes an item from the cart
+function removeItemFromCart(event) {
+    var buttonClicked = event.target;
+    buttonClicked.parentElement.parentElement.remove();
+    updateCartTotal();
+}
+
+//updates total price in real time
+function quantityChanged(event) {
+    var input = event.target;
+    if (isNaN(input.value) || input.value <= 0) {
+        input.value = 1;
+    }
+    updateCartTotal();
+}
+
+//Update cart total after changes have been made
+function updateCartTotal() {
+    var cartItemContainer = document.getElementsByClassName('cart-items')[0];
+    var cartRows = cartItemContainer.getElementsByClassName('cart-row');
+    var total = 0;
+    for (var i = 0; i < cartRows.length; i++) {
+        var cartRow = cartRows[i];
+        var priceElement = cartRow.getElementsByClassName('cart-price')[0];
+        var quantityElement = cartRow.getElementsByClassName('cart-quantity-input')[0];
+        var price = parseFloat(priceElement.innerText.replace('£', ''));
+        var quantity = quantityElement.value;
+        console.log(price * quantity);
+        total = total + (price*quantity);
+    }
+    total = Math.round(total*100) / 100;
+    totalPrice = total;
+    document.getElementsByClassName('cart-total-price')[0].innerText = '£' + total;
+}
