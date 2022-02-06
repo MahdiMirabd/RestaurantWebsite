7924e33ad87577d9b0a4db13ef81c227d01a2be1
22am <amirreza.amiri.2020@live.rhul.ac.uk>
2022-02-06 14:29:55 +0000

    Small changes to the buttons' styling.
    They now have rounded corners.

--- src/main/resources/static/javascript/header.js
+++ src/main/resources/static/javascript/header.js
@@ -39,6 +39,7 @@ class Header extends HTMLElement {
                     'position:absolute;' +
                     'top:20px;' +
                     'right:200px;' +
+                    '  border-radius: 10px;'  +
                 '} ' +
                   '.shopping-cart-button:hover {' +
                     'background-color: DodgerBlue;' +
