cb47f62c46d93d690c3ea535506522562e9ca205
M-D-Junior <59103723+M-D-Junior@users.noreply.github.com>
2022-02-08 21:23:59 +0000

    Deleted Alert.sql
    I refactored the code to make the framework create the database instead of manually creating the database so I can use it straight from the frame work instead of manually calling the file every time it is needed.

--- sql/alertMsg.sql
+++
@@ -1,5 +0,0 @@
-DROP TABLE IF EXISTS messagesTable CASCADE ;
-
-CREATE TABLE messagesTable ( id SERIAL PRIMARY KEY , message VARCHAR (225) NOT NULL , readFlag VARCHAR (50) NOT NULL );
-
-select id,message,readFlag from messagesTable where readFlag = false ;
