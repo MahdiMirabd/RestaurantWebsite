8d71bd785462085171adeb5dcf7a126268df9831
hamcheesycows <liamwilson935@outlook.com>
2022-02-09 18:41:16 +0000

    setup the login for future development
    The login screen was not setup for multiple department (ie. the Kitchen staff need their own login) so I implemented an intuitive way for the staff to login.

--- src/main/resources/static/javascript/login.js
+++ src/main/resources/static/javascript/login.js
@@ -1,8 +1,12 @@
 <!--Function to check password the already set password is admin-->
 function checkPass() {
     const confirmPassword = "admin";
+    const confirmWaiter = "waiter";
+
     let password = document.getElementById("pass").value;
-    if (password == confirmPassword) {
+    let department = document.getElementById("department").value;
+
+    if (password == confirmPassword && department == confirmWaiter){
         window.location="waiter";
     }
     else{
