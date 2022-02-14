ab3957025d21ef63f54f93af78a141f62d78ef92
Hestre <zhac092@live.rhul.ac.uk>
2022-02-14 16:01:18 +0000

    Create backend PUT request to change order status to 'ready'.

--- src/main/java/uk/ac/rhul/cs2810/restaurantsystem/controller/KitchenController.java
+++ src/main/java/uk/ac/rhul/cs2810/restaurantsystem/controller/KitchenController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import uk.ac.rhul.cs2810.restaurantsystem.repository.OrderRepository;
@@ -32,6 +33,21 @@ public class KitchenController {
     public String findAllOrders(Model model) {
         model.addAttribute("orders", orderRepository.findOrders("confirmed"));
         return "kitchen";
+    }
+
+    /**
+     * Changes the status of an order from confirmed to ready.
+     *
+     * @param id the order number
+     * @param model the database table to query
+     * @return the view back to the waiter page
+     */
+    @RequestMapping(value = "/kitchen/{id}" , method = {RequestMethod.GET, RequestMethod.PUT})
+    public String confirmOrder(@PathVariable long id, Model model){
+        Order order = orderRepository.getById(id);
+        order.setStatus("ready");
+        orderRepository.save(order);
+        return findAll(model);
     }
 
 }
