3eb854bf59624031c7748850d148cc138b7e11ee
Hestre <zhac092@live.rhul.ac.uk>
2022-02-24 11:46:24 +0000

    Style: Add icons to buttons on edit-menu page

--- src/main/resources/static/javascript/editMenu.js
+++ src/main/resources/static/javascript/editMenu.js
@@ -1,14 +1,4 @@
-//Checks if a checkbox has changed since loading the page
-$(document).ready(function() {
-    /*$('.menu-group span').each(function() {
-        //span.after('<span>' + span.text() + '</span>')
-        if($('#menu-item span').text()== false) {
-            document.getElementById("available").style.display= 'none';
-        } else  {
-            document.getElementById("unavailable").style.display= 'none';
-        }
-    });*/
-});
+
 
 function checkboxChanged(event) {
     let input = event.target;
