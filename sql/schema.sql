5d5c23da9b671f10441a6d77f108b9a618aafc6c
Hestre <zhac092@live.rhul.ac.uk>
2022-02-24 10:33:48 +0000

    Bug: Fix bug with menu item image visibility
    The images for fajita and enchilada was not being rendered on the webpage.

--- sql/schema.sql
+++ sql/schema.sql
@@ -20,7 +20,7 @@ VALUES  ('Burrito', 20.50, 90, 'beef, cheese and spicy rice, beans', ARRAY ['Glu
         ('Nacho', 10.50, 100, 'cheese, tomato, guacamole, sour cream', ARRAY ['Gluten','Eggs','Milk'], '/img/nacho.jpg', TRUE),
         ('Taco', 15, 120, 'beef, cheese, tomato', ARRAY ['Gluten', 'soy','Milk'], '/img/taco.jpg', TRUE),
         ('Quesadilla', 13, 80, 'chicken, cheese', ARRAY ['Gluten','Mustard','Milk'], '/img/quesadilla.jpg', TRUE),
-        ('Fajita', 14, 80, 'chicken, cheese, salad, sour cream', ARRAY ['Gluten','Mustard','Milk'], '/img/fajitas.jpg', TRUE),
+        ('Fajita', 14, 80, 'chicken, cheese, salad, sour cream', ARRAY ['Gluten','Mustard','Milk'], '/img/fajita.jpg', TRUE),
         ('Enchilada', 12, 80, 'beef, cheese, salad', ARRAY ['Gluten','Soy','Milk'], '/img/enchiladas.jpg', TRUE);
 
 
