e73adca522def8ccbc493f022579624959e30493
Hestre <zhac092@live.rhul.ac.uk>
2022-03-02 14:47:31 +0000

    Create query to Get all occupied tables from the database.

--- src/main/java/uk/ac/rhul/cs2810/restaurantsystem/controller/PaymentController.java
+++ src/main/java/uk/ac/rhul/cs2810/restaurantsystem/controller/PaymentController.java
@@ -1,16 +1,25 @@
 package uk.ac.rhul.cs2810.restaurantsystem.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.Banner;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
+import uk.ac.rhul.cs2810.restaurantsystem.repository.TableRepository;
 
 @Controller
 public class PaymentController {
+
+    @Autowired
+    TableRepository tableRepository;
+
     @RequestMapping(value = "/payment", method = RequestMethod.GET)
-    public ModelAndView payment() {
-        ModelAndView modelAndView = new ModelAndView();
-        modelAndView.setViewName("payment");
-        return modelAndView;
+    public String findByStatus(Model model) {
+        model.addAttribute("tables", tableRepository.findByStatus(false));
+        return "payment";
     }
+
+
 }
--- src/main/java/uk/ac/rhul/cs2810/restaurantsystem/repository/TableRepository.java
+++ src/main/java/uk/ac/rhul/cs2810/restaurantsystem/repository/TableRepository.java
@@ -1,6 +1,10 @@
 package uk.ac.rhul.cs2810.restaurantsystem.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import uk.ac.rhul.cs2810.restaurantsystem.model.Tables;
 
@@ -15,4 +19,6 @@ import uk.ac.rhul.cs2810.restaurantsystem.model.Tables;
 @Repository
 public interface TableRepository extends JpaRepository<Tables, Long> {
 
+    @Query("SELECT t FROM tables  t WHERE t.available = :stat")
+    List<Tables> findByStatus(@Param("stat") boolean stat);
 }
