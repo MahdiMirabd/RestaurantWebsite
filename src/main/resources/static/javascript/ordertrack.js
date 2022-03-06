89248b2aa38e9ec6207f5e8a180b94242ca7a91e
yxngrei <74990865+yxngrei@users.noreply.github.com>
2022-03-06 19:36:31 +0000

    Added a basic timer to the page

---
+++ src/main/resources/static/javascript/ordertrack.js
@@ -0,0 +1,18 @@
+window.onload = function() {
+    setTime()
+  };
+var minutesLabel = document.getElementById("minutes");
+var secondsLabel = document.getElementById("seconds");
+var totalSeconds = 0;
+setInterval(setTime, 1000);
+  function setTime() {
+    ++totalSeconds;
+    secondsLabel.innerHTML = pad(totalSeconds % 60);
+    minutesLabel.innerHTML = pad(parseInt(totalSeconds / 60));
+  }
+  var sec = 0;
+  function pad ( val ) { return val > 9 ? val : "0" + val; }
+  setInterval( function(){
+      $("#seconds").html(pad(++sec%60));
+      $("#minutes").html(pad(parseInt(sec/60,10)));
+  }, 1000);
