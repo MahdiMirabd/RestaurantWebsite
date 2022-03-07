ea2b1056ef6088bf6b99273915df8b3835fd93c7
Hestre <zhac092@live.rhul.ac.uk>
2022-03-07 16:01:57 +0000

    Disable payment form until table number is entered.

---
+++ src/main/resources/static/javascript/payment.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+    $("#name").prop("disabled",true);
+    $("#cardnumber").prop("disabled",true);
+    $("#month").prop("disabled",true);
+    $("#year").prop("disabled",true);
+    $("#securitycode").prop("disabled",true);
+
+});
