8db6f19d224b7b87b83120b73dae10178d93bfb8
Hestre <zhac092@live.rhul.ac.uk>
2022-03-09 20:34:01 +0000

    feat: Menu filtering concept now working. need to be fully implemented.

--- src/main/resources/static/javascript/menu.js
+++ src/main/resources/static/javascript/menu.js
@@ -4,8 +4,9 @@ function filterOptions() {
     for (let x = 0; x < divs.length; x++) {
         let div = divs[x];
         //let content = div.innerHTML.trim();
-        let content = document.getElementById('allergen-list').innerText;
-        if (content.includes("Celery")) {
+        let content = div.innerHTML.trim();
+        console.log(content);
+        if (content.toLowerCase().includes("celery")) {
             div.style.display = 'none';
         }
     }
