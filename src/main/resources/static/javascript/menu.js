b9dc644bbb59b6334397b0568efcd6b34b442b7b
Rayan111001 <zjac382@live.rhul.ac.uk>
2022-03-10 16:17:39 +0000

    Completed menu filtering functionality, allows filtering for all allergens.

--- src/main/resources/static/javascript/menu.js
+++ src/main/resources/static/javascript/menu.js
@@ -1,12 +1,15 @@
-function filterOptions() {
+function filterOptions(allergy) {
     let divs = document.getElementsByClassName('menu-item');
 
     for (let x = 0; x < divs.length; x++) {
         let div = divs[x];
-        //let content = div.innerHTML.trim();
         let content = div.innerHTML.trim();
         console.log(content);
-        if (content.toLowerCase().includes("celery")) {
+        console.log(allergy);
+        if (allergy == "no-filters"){
+            location.reload();
+        }
+        if (content.toLowerCase().includes(allergy)) {
             div.style.display = 'none';
         }
     }
