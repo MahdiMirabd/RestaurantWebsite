e0ee9d85087fe9906fc5ba41dd4c193afff083ed
Hestre <zhac092@live.rhul.ac.uk>
2022-03-20 23:22:06 +0000

    Refactor: delete schema.sql, use component class for insertion
    We are now using the command line runner to insert data into the database. reducing the amount of steps the client needs to run the program.

--- src/main/java/uk/ac/rhul/cs2810/restaurantsystem/component/DatabaseInfo.java
+++ src/main/java/uk/ac/rhul/cs2810/restaurantsystem/component/DatabaseInfo.java
@@ -3,7 +3,9 @@ package uk.ac.rhul.cs2810.restaurantsystem.component;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
+import uk.ac.rhul.cs2810.restaurantsystem.model.Menu;
 import uk.ac.rhul.cs2810.restaurantsystem.model.Tables;
+import uk.ac.rhul.cs2810.restaurantsystem.repository.MenuRepository;
 import uk.ac.rhul.cs2810.restaurantsystem.repository.TableRepository;
 
 /**
@@ -14,10 +16,16 @@ public class DatabaseInfo implements CommandLineRunner {
     @Autowired
     TableRepository tableRepository;
 
+    @Autowired
+    MenuRepository menuRepository;
+
     @Override
     public void run(String... args) throws Exception {
         if(tableRepository.count() == 0) {
             insertTableRecords();
+        }
+        if(menuRepository.count() == 0) {
+            insertMenuItems();
         }
     }
 
@@ -38,5 +46,27 @@ public class DatabaseInfo implements CommandLineRunner {
             // increment j if it is less than 2 else reset to 0
             j = (j == waiters.length - 1) ? 0 : j + 1;
         }
+    }
+
+    public void insertMenuItems() {
+        Menu burrito = new Menu(1,"Burrito",20.5f, 90, "beef, cheese and spicy rice, beans",
+                "Gluten, Milk, Celery, Soy", "/img/burrito.jpg", true );
+        Menu nacho = new Menu(2, "Nacho", 10.5f, 100, "cheese, tomato, guacamole, sour cream",
+                    "Gluten, Eggs, Milk","/img/nacho.jpg", true);
+        Menu taco = new Menu(3, "Taco", 15f, 120, "beef, cheese, tomato",
+                    "Gluten, Soy, Milk", "/img/taco.jpg", true);
+        Menu quesadilla = new Menu(4, "Quesadilla", 13f, 80, "chicken, cheese",
+                        "Gluten, Mustard, Milk", "/img/quesadilla.jpg", true);
+        Menu fajita = new Menu(5, "Fajita", 14f, 80, "chicken, cheese, salad, sour cream",
+                    "Gluten, Mustard, Milk", "/img/fajita.jpg", true );
+        Menu enchilada = new Menu(6, "Enchilada", 12f, 80, "beef, cheese, salad",
+                        "Gluten, Soy, Milk", "/img/enchiladas.jpg", true);
+
+        menuRepository.save(burrito);
+        menuRepository.save(nacho);
+        menuRepository.save(taco);
+        menuRepository.save(quesadilla);
+        menuRepository.save(fajita);
+        menuRepository.save(enchilada);
     }
 }
